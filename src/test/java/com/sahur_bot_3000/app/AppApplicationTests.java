07c1a837ec9c6b125402a833374b2ab84fbc7e21
Octavian Regatun <octavian.regatun@outlook.com>
2025-05-29 18:30:50 +0300

    feat: init repo

---
+++ src/test/java/com/sahur_bot_3000/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahur_bot_3000.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
